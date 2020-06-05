e4466c066d52d610c9cf5e1758a0d06b04552906
myrin <gebruiker@MacBook-Pro-van-Gebruiker.local>
2020-06-05 16:26:58 +0200

    transition

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const list = document.querySelector('.items');
 const background = document.getElementById('aanbevolen__background');
 const bg1 = document.querySelector('.bg-1');
 const bg2 = document.querySelector('.bg-2');
+const bg3 = document.querySelector('.bg-3');
 let count = 1;
 let bount = 2
 
@@ -20,6 +21,13 @@ voorbeelditem.addEventListener('click', function(){
 })
 
 setInterval(() => {
-    background.classList.remove('bg-1');
-    background.classList.add('bg-2');
-}, 6000);
+    count++;
+    if(count === 4){
+        count = 1;
+    }
+    console.log('blablaad');
+    bg1.style.opacity = '0';
+    bg2.style.opacity = '0';
+    bg3.style.opacity = '0';
+    document.querySelector(`.bg-${count}`).style.opacity = '1';
+}, 7000);
